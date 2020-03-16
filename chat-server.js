37e2455708096075eeac7b744f07c1b35d449a23
yitongwang <tongyiwangzaimilk@gmail.com>
2020-03-16 16:38:22 -0500

    first commit

--- chat-server.js
+++ chat-server.js
@@ -1,10 +1,49 @@
 // Require the packages we will use:
 let http = require("http"),
     socketio = require("socket.io"),
-    fs = require("fs");
+    fs = require("fs"),
+    url = require('url'),
+    path = require('path'),
+    mime = require('mime'),
+    path = require('path');
 
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
 let app = http.createServer(function(req, resp){
+
+    var filename = path.join(__dirname, "static", url.parse(req.url).pathname);
+    (fs.exists || path.exists)(filename, function(exists){
+        if (exists) {
+            fs.readFile(filename, function(err, data){
+                if (err) {
+                    // File exists but is not readable (permissions issue?)
+                    resp.writeHead(500, {
+                        "Content-Type": "text/plain"
+                    });
+                    resp.write("Internal server error: could not read file");
+                    resp.end();
+                    return;
+                }
+
+                // File exists and is readable
+                var mimetype = mime.getType(filename);
+                resp.writeHead(200, {
+                    "Content-Type": mimetype
+                });
+                resp.write(data);
+                resp.end();
+                return;
+            });
+        }else{
+            // File does not exist
+            resp.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            resp.write("Requested file not found: "+filename);
+            resp.end();
+            return;
+        }
+    });
+
     // This callback runs when a new connection is made to our HTTP server.
 
     fs.readFile("chat.html", function(err, data){
